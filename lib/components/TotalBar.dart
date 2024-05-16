b4bde69fec7cf54cfd271b153cbeade3a8938057
marianyuzu <112700576+marianyuzu@users.noreply.github.com>
2024-05-17 07:18:51 +1000

    add customise bottom sheet

---
+++ lib/components/TotalBar.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class TotalBar extends StatelessWidget {
+  final double totalPrice;
+
+  const TotalBar({Key? key, required this.totalPrice}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 1,
+            blurRadius: 6,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "Total KES $totalPrice",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Colors.black,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Add your onPressed logic here
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.orange,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18),
+              ),
+            ),
+            child: Text(
+              "+ Add",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/item.dart
+++ lib/item.dart
@@ -1,17 +1,58 @@
 class Item {
-  String name; //ID Unique de l'image
+  String name;
   String img;
   String category;
+  int quantity;
+  int sugarLevel;
+  Map<String, bool> addOns; 
+  Map<String, double> addOnPrices;
 
-  getName() {
-    return name;
+  Item(this.name, this.img, this.category, {
+      this.quantity = 1,
+      this.sugarLevel = 1,
+      Map<String, bool>? addOns,
+      Map<String, double>? addOnPrices
+  }) : this.addOns = addOns ?? {},
+       this.addOnPrices = addOnPrices ?? {
+         'Cookie': 11.0,
+         'Marshmallow': 11.0,
+         'Chocolate': 11.0
+       };
+
+  String getName() => name;
+
+  String getImg() => img;
+
+  void incrementQuantity() {
+    quantity++;
   }
 
-  getImg() {
-    return img;
+  void decrementQuantity() {
+    if (quantity > 1) quantity--;
   }
 
-  Item(this.name, this.img, this.category);
+  void setSugarLevel(int level) {
+    sugarLevel = level;
+  }
+
+  void toggleAddOn(String addOn) {
+    if (addOns.containsKey(addOn)) {
+      addOns[addOn] = !addOns[addOn]!;
+    }
+  }
+
+  double getTotalPrice() {
+    double totalPrice = 0.0;
+    totalPrice += 100.0;
+
+    addOns.forEach((addOn, included) {
+      if (included) {
+        totalPrice += addOnPrices[addOn]!;
+      }
+    });
+
+    return totalPrice * quantity;
+  }
 }
 
 class Category {
