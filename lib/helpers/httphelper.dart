0827b53bf14c507acbe7f983c9ddccbb311c3afa
Larry Yu <yuyang8986@gmail.com>
2024-08-13 22:20:52 +1000

    1213

--- lib/helpers/httphelper.dart
+++ lib/helpers/httphelper.dart
@@ -4,7 +4,7 @@ import 'package:mcdo_ui/models/item-category.dart';
 import 'package:mcdo_ui/models/order.dart';
 
 class HttpClientHelper {
-  final String baseUrl = "http://localhost:5224/orders";
+  final String baseUrl = "http://localhost:5000/orders";
 
   Future<http.Response> httpGet(String endpoint) async {
     final response = await http.get(Uri.parse('$baseUrl$endpoint'));
