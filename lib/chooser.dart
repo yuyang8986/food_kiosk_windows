decb608a81cabb6fee248bc89a79908bcce19114
yy <yuyang8986@email.com>
2024-09-09 20:36:10 +1000

    1213

--- lib/chooser.dart
+++ lib/chooser.dart
@@ -18,34 +18,58 @@ import 'package:mcdo_ui/payment.dart';
 import 'package:mcdo_ui/printer_info.dart';
 import 'package:print_image_generate_tool/print_image_generate_tool.dart';
 
-
 class Navigation {
   static final router = FluroRouter();
   static bool _routesDefined = false;
+  static Order? currentOrder; // Track the current order
+  static String? orderType; 
 
   static void initPaths(order, type, printerInfo) {
-    if (!_routesDefined) {
-      var chooserHandler = Handler(
-        handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
-          return Chooser(type: type, printerInfo: printerInfo,);
-        },
-      );
-
-      var paymentHandler = Handler(
-        handlerFunc: (BuildContext? context, Map<String, dynamic> params) {
-          return Payment(order: order, type: type, printerInfo: printerInfo,);
-        },
-      );
-
-      void defineRoutes(FluroRouter router) {
-        router.define("/", handler: chooserHandler);
-        router.define("payment", handler: paymentHandler);
-      }
-
-      defineRoutes(router);
-      _routesDefined = true;
-    }
+  if (currentOrder != order) {
+    _routesDefined = false;
+    currentOrder = order; // Update current order
   }
+
+  // if (!_routesDefined) {
+    var chooserHandler = Handler(
+      handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
+        return Chooser(
+          type: type,
+          printerInfo: printerInfo,
+        );
+      },
+    );
+
+    var paymentHandler = Handler(
+      handlerFunc: (BuildContext? context, Map<String, dynamic> params) {
+        return Payment(
+          order: order,
+          type: type,
+          printerInfo: printerInfo,
+        );
+      },
+    );
+
+    currentOrder = order;
+    orderType = type;
+
+    try {
+      // Try defining the routes, catch exceptions for already-defined routes
+      router.define("/", handler: chooserHandler);
+    } catch (e) {
+      print("Route '/' is already defined: $e");
+    }
+
+    try {
+      router.define("payment", handler: paymentHandler);
+    } catch (e) {
+      print("Route 'payment' is already defined: $e");
+    }
+
+    _routesDefined = true;
+  // }
+}
+
 }
 
 class Chooser extends StatefulWidget {
@@ -61,19 +85,20 @@ class Chooser extends StatefulWidget {
 class _MyChooserState extends State<Chooser> {
   late List<ItemCategory> categories = [];
   List<Item> filteredItems = [];
-  Order order = Order(
-    orderPrice: 0.0,
-    orderItems: [],
-    orderType: '',
-    orderDescription: '',
-    orderName: '',
-  );
+  late Order order;
   String category = "All";
   late Future<List<ItemCategory>> futureMenu;
 
   @override
   void initState() {
     super.initState();
+    order = Order(
+      orderPrice: 0.0,
+      orderItems: [],
+      orderType: '',
+      orderDescription: '',
+      orderName: '',
+    );
     order.orderType = widget.type;
     futureMenu = HttpClientHelper().fetchMenu();
   }
@@ -108,37 +133,38 @@ class _MyChooserState extends State<Chooser> {
   }
 
   showCustomizeSheet(int position) {
-  showModalBottomSheet(
-    isScrollControlled: true,
-    context: context,
-    builder: (context) => CustomizeItemSheet(
-      orderItem: OrderItem(
-        item: filteredItems[position],
-        order: order,
-        quantity: 1,
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (context) => CustomizeItemSheet(
+        orderItem: OrderItem(
+          item: filteredItems[position],
+          order: order,
+          quantity: 1,
+        ),
+        position: position,
+        onAddToCart: (OrderItem orderItem) {
+          setState(() {
+            int existingIndex = order.orderItems.indexWhere(
+              (oi) => oi.item.itemId == orderItem.item.itemId,
+            );
+
+            if (existingIndex >= 0) {
+              // Update the existing item in the cart
+              order.orderItems[existingIndex].quantity += orderItem.quantity;
+            } else {
+              // Add the new item to the cart
+              order.orderItems.add(orderItem);
+            }
+
+            calculateTotal();
+          });
+        },
       ),
-      position: position,
-      onAddToCart: (OrderItem orderItem) {
-        setState(() {
-          int existingIndex = order.orderItems.indexWhere(
-            (oi) => oi.item.itemId == orderItem.item.itemId,
-          );
+    );
+  }
 
-          if (existingIndex >= 0) {
-            // Update the existing item in the cart
-            order.orderItems[existingIndex].quantity += orderItem.quantity;
-          } else {
-            // Add the new item to the cart
-            order.orderItems.add(orderItem);
-          }
-
-          calculateTotal();
-        });
-      },
-    ),
-  );
-}
-Future<void> _onPictureGenerated(PicGenerateResult data) async {
+  Future<void> _onPictureGenerated(PicGenerateResult data) async {
     final printTask = data.taskItem;
 
     //指定的打印机
@@ -174,76 +200,75 @@ Future<void> _onPictureGenerated(PicGenerateResult data) async {
   }
 
   @override
-Widget build(BuildContext context) {
-  var scaffold = Scaffold(
-    body: Stack(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 0.0), // Add 100px top padding
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                flex: 2,
-                child: FutureBuilder<List<ItemCategory>>(
-                  future: futureMenu,
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(child: CircularProgressIndicator());
-                    } else if (snapshot.hasError) {
-                      return Center(child: Text('Error: ${snapshot.error}'));
-                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                      return Center(child: Text('No data available'));
-                    } else {
-                      categories = snapshot.data!;
-                      return LeftBar(
-                        categories: categories,
-                        onCategorySelected: handleCategorySelected,
-                      );
-                    }
-                  },
-                ),
-              ),
-              Expanded(
-                flex: 5,
-                child: RightBar(
-                  filteredItems: filteredItems,
-                  order: order,
-                  onViewCartPressed: () => showCart(widget.printerInfo),
-                  onAddToCartPressed: showCustomizeSheet,
-                ),
-              ),
-            ],
-          ),
-        ),
-        Positioned(
-          top: 1.0,
-          child: Container(
-            padding: EdgeInsets.only(left: 15.0, right: 15.0),
-            width: MediaQuery.of(context).size.width,
+  Widget build(BuildContext context) {
+    var scaffold = Scaffold(
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 0.0), // Add 100px top padding
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                fui.Image.asset(
-                  'assets/logo.png',
-                  height: 100.0,
-                  fit: BoxFit.cover,
+              children: <Widget>[
+                Expanded(
+                  flex: 2,
+                  child: FutureBuilder<List<ItemCategory>>(
+                    future: futureMenu,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasError) {
+                        return Center(child: Text('Error: ${snapshot.error}'));
+                      } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                        return Center(child: Text('No data available'));
+                      } else {
+                        categories = snapshot.data!;
+                        return LeftBar(
+                          categories: categories,
+                          onCategorySelected: handleCategorySelected,
+                        );
+                      }
+                    },
+                  ),
+                ),
+                Expanded(
+                  flex: 5,
+                  child: RightBar(
+                    filteredItems: filteredItems,
+                    order: order,
+                    onViewCartPressed: () => showCart(widget.printerInfo),
+                    onAddToCartPressed: showCustomizeSheet,
+                  ),
                 ),
               ],
             ),
           ),
-        ),
-      ],
-    ),
-  );
-  
-  return PrintImageGenerateWidget(
-       contentBuilder: (context) {
-       return scaffold;
-       },
-       onPictureGenerated: _onPictureGenerated,  //下面说明
-     );
-}
+          Positioned(
+            top: 1.0,
+            child: Container(
+              padding: EdgeInsets.only(left: 15.0, right: 15.0),
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  fui.Image.asset(
+                    'assets/logo.png',
+                    height: 100.0,
+                    fit: BoxFit.cover,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
 
+    return PrintImageGenerateWidget(
+      contentBuilder: (context) {
+        return scaffold;
+      },
+      onPictureGenerated: _onPictureGenerated, //下面说明
+    );
+  }
 
   void showCart(printerInfo) {
     showModalBottomSheet(
